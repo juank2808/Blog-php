ace024bda700200615cf96ce9a2d204fbc33c5a6
juank2808 <Colombia1895>
2021-06-16 16:05:19 +0200

    adding somethings

--- views/index.view.php
+++ views/index.view.php
@@ -1,3 +1,13 @@
-<?php
-echo ("HI");
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Blog</title>
+  </head>
+  <body>
+    <h1>Blog</h1>
+  </body>
+</html>
