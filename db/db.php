a043b22cca858628435e189e327dcf311bc008c2
juank2808 <Colombia1895>
2021-06-16 16:07:18 +0200

    DB connect

--- db/db.php
+++ db/db.php
@@ -1,1 +1,22 @@
-<?php  ?>
+<?php
+
+/**
+ *
+ */
+class Connection
+{
+
+  public function connect(){
+    try {
+    $conn = new PDO('mysql:host=127.0.0.1;dbname=PDO-php','root','');
+
+    } catch (PDOException $e) {
+      echo "Error:". $e->getMessage();
+      die();
+    }
+    return $conn;
+  }
+}
+
+
+?>
