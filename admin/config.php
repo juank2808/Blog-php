1a5ebc1af3876e7b89ae339596e6bc98d62f2f8a
juank2808 <Colombia1895>
2021-06-17 12:04:00 +0200

    FOOTER TPL

--- admin/config.php
+++ admin/config.php
@@ -2,4 +2,5 @@
 define ('P_PATH', '/');
 define ('PATH_INDEX', 'index.php');
 define ('PATH_HEADER_TPL','templates/header.tpl.php');
+define ('PATH_FOOTER_TPL', 'templates/footer.tpl.php')
  ?>
---
+++ templates/footer.tpl.php
@@ -0,0 +1,7 @@
+<footer class="footer mt-auto py-3 bg-dark ">
+  <div class="container">
+    <div class="row">
+      <span class="text-muted">My BLOG with PHP <a href="https://es.linkedin.com/in/juan-camilo-amaya-florez-b95084138"><i class="bi bi-linkedin"></i></></span>
+    </div>
+  </div>
+</footer>
