25b2042e2bfb2a7c5dd419350decb79b50bfe371
juank2808 <Colombia1895>
2021-07-01 12:38:05 +0200

    get only article

---
+++ views/article.view.php
@@ -0,0 +1,7 @@
+  <?php require PATH_HEADER_TPL; ?>
+  <div class="container">
+    <div class="row">
+      <h1 class="text-center"><?php echo $article['a_title']; ?></h1>
+    </div>
+  </div>
+  <?php require PATH_FOOTER_TPL; ?>
