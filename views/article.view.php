b02697c4ba2e0003a7817c03a46f48bc885e8bc4
juank2808 <Colombia1895>
2021-07-01 13:11:28 +0200

    showing article :)

--- views/article.view.php
+++ views/article.view.php
@@ -1,7 +1,16 @@
   <?php require PATH_HEADER_TPL; ?>
-  <div class="container">
-    <div class="row">
-      <h1 class="text-center"><?php echo $article['a_title']; ?></h1>
+  <div class="paper-effe container">
+    <div class="row justify-content-md-center">
+      <h1 class=""><?php echo $article['a_title']; ?></h1>
+      <h6 class="gray"><?php echo $article['a_date']; ?></h6>
+
+      <div class="col-12">
+          <div class="other-effe">
+            <img src="<?php echo PATH_PICS.'prov-image.png'; ?>" class="img-fluid" alt="">
+          </div>
+          <?php  echo $article['a_text'];?>
+          <br/>
+      </div>
     </div>
   </div>
   <?php require PATH_FOOTER_TPL; ?>
