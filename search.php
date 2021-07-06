76513c96ac34547b9e0adc6135bd2e301fe882e7
juank2808 <Colombia1895>
2021-07-06 14:43:35 +0200

    add coment :) try with JS my friend

--- search.php
+++ search.php
@@ -4,6 +4,7 @@ require 'admin/config.php';
 // require 'admin/functions.php';
 
 var_dump($_GET);
+//i will try with JS to show data :)
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['search'])) {
   $srch_a = clean_data($_GET['search']);
   $article  = new Article('','','','','');
